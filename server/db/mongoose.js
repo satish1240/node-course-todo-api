b61b976fe66dfdc1b7f7c725d3f482272bd70e48
satish1240 <babuk0930@gmail.com>
2019-01-05 04:33:26 +0000

    setup separate dev and test environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://18.220.60.82:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 
 module.exports = { mongoose };
